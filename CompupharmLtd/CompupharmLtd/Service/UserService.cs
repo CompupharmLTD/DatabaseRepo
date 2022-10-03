ba0a69fe5ea5d71ec7c871cf6689230b9d952ddf
ritzyRosabel <rosabelolugbenga@yahoo.com>
2022-10-03 11:24:26 -0500

    first commit

---
+++ CompupharmLtd/CompupharmLtd/Service/UserService.cs
@@ -0,0 +1,19 @@
+﻿using CompupharmLtd.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompupharmLtd.Service
+{
+    public class UserService
+    {
+        internal static LoginStatus Login(LoginUser cred)
+        {
+            if (cred.UserName != null)
+            {
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
