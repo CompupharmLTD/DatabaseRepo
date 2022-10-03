583a7e01ed4cfc3a6645e2a3e79fa34b47067653
ritzyRosabel <rosabelolugbenga@yahoo.com>
2022-10-03 11:12:19 -0500

    Add project files.

---
+++ CompupharmLtd/Model/LoginStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompupharmLtd.Model
+{
+    public class LoginStatus
+    {
+
+        public string status { get; set; }
+        public int statusCode { get; set; }
+    }
+}
---
+++ CompupharmLtd/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompupharmLtd.Model
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyAddress { get; set; }
+        public string CompanyPhone { get; set; }
+        public Byte CompanyCertificate { get; set; }
+    }
+
+
+    public class LoginUser
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
